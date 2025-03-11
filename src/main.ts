c03c714f535ab0b4046f6e62d716018c320b5504
Mikael Kristiansson <mikael.kristiansson@nordnet.se>
2025-03-11 21:39:44 +0100

    chore: remove coverage folder

--- src/main.ts
+++ src/main.ts
@@ -3,3 +3,4 @@ export { isValidDanishSSN } from './ssn/dk';
 export { isValidNorwegianSSN } from './ssn/no';
 export { isValidFinnishSSN } from './ssn/fi';
 export { isValidDate, isLeapYear } from './date/is-valid';
+export { accountNumberCheck } from './economy/bank-se';
